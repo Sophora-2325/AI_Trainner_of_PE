cff9cee29e4e97411e4376e1299472116d4dd157
Sophora <cn_qkdwtp@outlook.com>
2026-05-28 13:52:10 +0800

    成功实现四方走路

---
+++ controllers/Walkforward/build/release/Walk.d
@@ -0,0 +1,12 @@
+build/release/Walk.o: Walk.cpp Walk.hpp \
+ D:\Webots/include/controller/cpp/webots/Robot.hpp \
+ D:/Webots/include/controller/c/webots/types.h \
+ D:\Webots/projects/robots/robotis/darwin-op/libraries/managers/include/RobotisOp2GaitManager.hpp \
+ D:\Webots/projects/robots/robotis/darwin-op/libraries/managers/include/RobotisOp2MotionManager.hpp \
+ D:\Webots/include/controller/cpp/webots/Accelerometer.hpp \
+ D:\Webots/include/controller/cpp/webots/Device.hpp \
+ D:\Webots/include/controller/cpp/webots/Gyro.hpp \
+ D:\Webots/include/controller/cpp/webots/Keyboard.hpp \
+ D:\Webots/include/controller/cpp/webots/LED.hpp \
+ D:\Webots/include/controller/cpp/webots/Motor.hpp \
+ D:\Webots/include/controller/cpp/webots/PositionSensor.hpp
---
+++ controllers/Walkforward/build/release/main.d
@@ -0,0 +1,3 @@
+build/release/main.o: main.cpp Walk.hpp \
+ D:\Webots/include/controller/cpp/webots/Robot.hpp \
+ D:/Webots/include/controller/c/webots/types.h
